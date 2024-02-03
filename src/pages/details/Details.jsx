7513ffb79f457c6a81332f50661c8ae9d1c4e637
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-02-03 12:48:11 +0530

    cas and video section done in details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import './styles.scss'
+import React from "react";
+import "./styles.scss";
 
-import useFetch from '../../hooks/useFetch'
-import { useParams } from 'react-router-dom'
-import DetailsBanner from './detailsBanner/DetailsBanner'
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideoSection";
 
 const Details = () => {
-
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
   return (
     <div>
-      <DetailsBanner video = {data?.results?.[0]} crew = {credits?.crew} />
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={credits?.loading} />
+      <VideosSection data={data} loading={loading} />
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
